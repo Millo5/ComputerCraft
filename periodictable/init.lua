cb281b1c671b4ef7eddba9aff76ce600587f06e4
Millo <23leeuw@gmail.com>
2025-08-14 00:28:42 +0200

    asdf

--- periodictable/init.lua
+++ periodictable/init.lua
@@ -1,6 +1,6 @@
 
 -- local url = "https://raw.githubusercontent.com/Millo5/ComputerCraft/master/src/lua/"
-local url = "https://raw.githubusercontent.com/Millo5/ComputerCraft/refs/heads/master/src/periodictable/"
+local url = "https://raw.githubusercontent.com/Millo5/ComputerCraft/refs/heads/master/periodictable/"
 local ids = { "init", "startup", "PeriodicTable" }
 
 for i, v in pairs(ids) do
@@ -14,6 +14,7 @@ for i, v in pairs(ids) do
     print("Downloading " .. v .. ".lua")
     local uniqueParam = "?nocache=" .. os.epoch("utc")
 
+    print(url .. v .. ".lua" .. uniqueParam)
     local h = http.get(url .. v .. ".lua" .. uniqueParam)
     local f = fs.open(v .. ".lua", "w")
     f.write(h.readAll())
